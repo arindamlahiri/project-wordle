95525a566682bba3765a9f3db0d5bed3534664b0
Arindam Lahiri <arindamlahiri17@gmail.com>
2023-09-11 00:12:29 +0530

    exercise 4 - add game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+	const guessResult = checkGuess(value, answer);
+
+	const arrayToMap = guessResult ? guessResult : range(5);
+
 	return (
 		<p className="guess">
-			{range(5).map((num) => (
-				<span key={num} className="cell">
-					{value ? value[num] : ''}
+			{arrayToMap.map((result, index) => (
+				<span key={index} className={`cell ${result.status || ''}`}>
+					{guessResult ? result.letter : ''}
 				</span>
 			))}
 		</p>
